a890759dfb1bf31893812278eeaf528c5b466786
VickyVsV <vikusiavsv@gmail.com>
2022-12-09 20:11:04 +0200

    done 1st task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imagesContainer = document.querySelector(`.gallery`);
+let imagesArray = [];
+let lightbox;
+
+galleryItems.forEach(({preview, original, description}) => {
+  const linkEl = document.createElement("a");
+  linkEl.className = "gallery__item";
+  linkEl.href = original;
+  
+  const imgEl = document.createElement("img");
+  imgEl.className = "gallery__image";
+  imgEl.src = preview;
+  imgEl.alt = description;
+  
+  imagesContainer.append(linkEl);
+  linkEl.append(imgEl);
+  imagesArray.push(imagesContainer);
+});
+
+lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: "alt",
+	captionDelay: 250,
+});
